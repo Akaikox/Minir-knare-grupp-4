72bdd70cd5b054d9f9f39fff11f8ad2b063ddc95
gustafthomsson <gustafthomsson@gmail.com>
2014-10-01 14:11:02 +0200

    test

--- miniraknare.py
+++ miniraknare.py
@@ -1,29 +1,30 @@
-__author__ = 'Andreas'
+# coding: utf-8
+author__ = 'Andreas'
 
 line = "Y"
 
 while line[0] not in ("n", "N"):
 
-val = input("Vad vill du göra? addition, subtraktion, multiplication eller division?")
+    val = input("Vad vill du gÃ¶ra? addition, subtraktion, multiplication eller division?")
 
-if val == ("division"):
-    siffra_1 = float(input("Skriv in tal 1:"))
-    siffra_2 = float(input("Skriv in tal 2:"))
-    print("Ditt svar är:", siffra_1 / siffra_2)
+    if val == ("division"):
+        siffra_1 = float(input("Skriv in tal 1:"))
+        siffra_2 = float(input("Skriv in tal 2:"))
+        print("Ditt svar Ã¤r:", siffra_1 / siffra_2)
 
-if val == ("addition"):
-    siffra_1 = float(input("Skriv in tal 1:"))
-    siffra_2 = float(input("Skriv in tal 2:"))
-    print("Ditt svar är:", siffra_1 + siffra_2)
+    if val == ("addition"):
+        siffra_1 = float(input("Skriv in tal 1:"))
+        siffra_2 = float(input("Skriv in tal 2:"))
+        print("Ditt svar Ã¤r:", siffra_1 + siffra_2)
 
-if val == ("subtraktion"):
-    siffra_1 = float(input("Skriv in tal 1:"))
-    siffra_2 = float(input("Skriv in tal 2:"))
-    print("Ditt svar är:", siffra_1 - siffra_2)
+    if val == ("subtraktion"):
+        siffra_1 = float(input("Skriv in tal 1:"))
+        siffra_2 = float(input("Skriv in tal 2:"))
+        print("Ditt svar Ã¤r:", siffra_1 - siffra_2)
 
-if val == ("multiplikation"):
-    siffra_1 = float(input("Skriv in tal 1:"))
-    siffra_2 = float(input("Skriv in tal 2:"))
-    print("Ditt svar är:", siffra_1 * siffra_2)
+    if val == ("multiplikation"):
+        siffra_1 = float(input("Skriv in tal 1:"))
+        siffra_2 = float(input("Skriv in tal 2:"))
+        print("Ditt svar Ã¤r:", siffra_1 * siffra_2)
 
-line = input("Vill du r�ka igen? j/n")
+line = input("Vill du räka igen? j/n")
